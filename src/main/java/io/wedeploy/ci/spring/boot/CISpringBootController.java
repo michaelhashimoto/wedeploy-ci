85e0b7439b7ca5097ac0bb95372f007143a3fb8f
michaelhashimoto <michael.hashimoto@liferay.com>
2017-09-23 16:49:57 -0700

    Add simple database interactions

--- src/main/java/io/wedeploy/ci/spring/boot/CISpringBootController.java
+++ src/main/java/io/wedeploy/ci/spring/boot/CISpringBootController.java
@@ -1,5 +1,10 @@
 package io.wedeploy.ci.spring.boot;
 
+import com.wedeploy.android.WeDeploy;
+import com.wedeploy.android.WeDeploy.Builder;
+import com.wedeploy.android.exception.WeDeployException;
+import com.wedeploy.android.transport.Response;
+
 import io.wedeploy.ci.jenkins.node.JenkinsMasters;
 import io.wedeploy.ci.util.EnvironmentUtil;
 
@@ -27,6 +32,54 @@ public class CISpringBootController {
 	@GetMapping("/masters")
 	public String masters() throws IOException {
 		return _jenkinsMasters.toString();
+	}
+
+	@GetMapping("/api")
+	public String restAPI() throws WeDeployException {
+		WeDeploy weDeploy = new WeDeploy.Builder().build();
+
+		/* Adding data */
+
+		JSONObject movie1JsonObject = new JSONObject()
+			.put("title", "Star Wars III")
+			.put("year", 2005)
+			.put("rating", 8.0);
+
+		JSONObject movie2JsonObject = new JSONObject()
+			.put("title", "Star Wars II")
+			.put("year", 2002)
+			.put("rating", 8.6);
+
+		JSONArray moviesJsonArray = new JSONArray()
+			.put(movie1JsonObject)
+			.put(movie2JsonObject);
+
+		Response response = weDeploy
+			.data("https://data-ci.wedeploy.io")
+			.create("movies", moviesJsonArray)
+			.execute();
+
+		System.out.println(response);
+
+		/* Retrieving data */
+
+		response = weDeploy
+			.data("https://data-ci.wedeploy.io")
+			.get("movies")
+			.execute();
+
+		System.out.println(response);
+
+		/* Deleting data */
+
+		response = weDeploy
+			.data("https://data-ci.wedeploy.io")
+			.delete("movies")
+			.execute();
+
+		System.out.println(response);
+
+		return response.toString();
 	}
 
 	private JenkinsMasters _jenkinsMasters;
